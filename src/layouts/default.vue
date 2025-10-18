3068f17c3708803313059a6e3362f1254b31b713
Nurbek Baizakov <n.baizakof@gmail.com>
2025-10-19 02:23:31 +0600

    style: adjust footer layout for better responsiveness

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -2,7 +2,9 @@
   <div class="p-4 py-8 w-full sm:max-w-112 mx-auto pb-20">
     <slot />
     <div class="fixed bottom-4 left-0 right-0 h-16 flex justify-center items-center">
-      <Footer class="max-w-96 mx-auto px-4 py-2 rounded-2xl backdrop-blur-xl bg-zinc-700/50" />
+      <Footer
+        class="mx-auto px-4 py-2 rounded-2xl backdrop-blur-xl bg-zinc-700/50 max-w-[calc(100%-32px)]"
+      />
     </div>
   </div>
 </template>
