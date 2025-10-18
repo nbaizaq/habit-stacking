73be9f710042a663b6773c2222ee4d269ac6867a
Nurbek Baizakov <n.baizakof@gmail.com>
2025-10-19 02:20:21 +0600

    style: update button variants and footer layout for improved UI

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,21 +2,21 @@
   <UFieldGroup class="w-full flex justify-center gap-2">
     <RouterLink to="/temporary" class="w-full">
       <UButton
-        :variant="router.currentRoute.value.path === '/temporary' ? 'subtle' : 'ghost'"
+        :variant="router.currentRoute.value.path === '/temporary' ? 'solid' : 'ghost'"
         class="w-full justify-center"
         >Routine</UButton
       >
     </RouterLink>
     <RouterLink to="/" class="w-full">
       <UButton
-        :variant="router.currentRoute.value.path === '/' ? 'subtle' : 'ghost'"
+        :variant="router.currentRoute.value.path === '/' ? 'solid' : 'ghost'"
         class="w-full justify-center"
         >Home</UButton
       >
     </RouterLink>
     <RouterLink to="/settings" class="w-full">
       <UButton
-        :variant="router.currentRoute.value.path === '/settings' ? 'subtle' : 'ghost'"
+        :variant="router.currentRoute.value.path === '/settings' ? 'solid' : 'ghost'"
         class="w-full justify-center"
         >Settings</UButton
       >
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,10 +1,8 @@
 <template>
-  <div class="p-4 w-full sm:max-w-112 mx-auto pb-12">
+  <div class="p-4 py-8 w-full sm:max-w-112 mx-auto pb-20">
     <slot />
-    <div
-      class="fixed bottom-0 left-0 right-0 h-12 flex justify-center items-center backdrop-blur-xl"
-    >
-      <Footer class="max-w-96 mx-auto" />
+    <div class="fixed bottom-4 left-0 right-0 h-16 flex justify-center items-center">
+      <Footer class="max-w-96 mx-auto px-4 py-2 rounded-2xl backdrop-blur-xl bg-zinc-700/50" />
     </div>
   </div>
 </template>
