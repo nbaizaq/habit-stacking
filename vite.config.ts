cb34900785a6bcf311f4eb30ff8668b0a8a20077
Nurbek Baizakov <n.baizakof@gmail.com>
2025-10-23 22:08:26 +0600

    chore: enable PWA support in vite.config.ts and remove manifest.json

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
-// import { VitePWA } from 'vite-plugin-pwa'
+import { VitePWA } from 'vite-plugin-pwa'
 import ui from '@nuxt/ui/vite'
 
 // https://vite.dev/config/
@@ -13,45 +13,45 @@ export default defineConfig({
     vue(),
     vueDevTools(),
     tailwindcss(),
-    // VitePWA({
-    //   registerType: 'autoUpdate',
-    //   workbox: {
-    //     skipWaiting: true,
-    //   },
-    //   devOptions: {
-    //     enabled: true,
-    //   },
-    //   manifest: {
-    //     name: 'Habit Stacking',
-    //     short_name: 'Habit S.',
-    //     icons: [
-    //       {
-    //         src: 'pwa-64x64.png',
-    //         sizes: '64x64',
-    //         type: 'image/png',
-    //       },
-    //       {
-    //         src: 'pwa-192x192.png',
-    //         sizes: '192x192',
-    //         type: 'image/png',
-    //       },
-    //       {
-    //         src: 'pwa-512x512.png',
-    //         sizes: '512x512',
-    //         type: 'image/png',
-    //       },
-    //       {
-    //         src: 'maskable-icon-512x512.png',
-    //         sizes: '512x512',
-    //         type: 'image/png',
-    //         purpose: 'maskable',
-    //       },
-    //     ],
-    //     theme_color: '#000000',
-    //     background_color: '#000000',
-    //     display: 'standalone',
-    //   },
-    // }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        skipWaiting: true,
+      },
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: 'Habit Stacking',
+        short_name: 'Habit S.',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+        theme_color: '#000000',
+        background_color: '#000000',
+        display: 'standalone',
+      },
+    }),
     ui({
       ui: {
         colors: {
